6b194a527920ce55b5cf940c5f95d1fc0576ac40
kousikalam786R <kousikalam786@gmail.com>
2025-08-02 02:28:50 +0530

    issue fixx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         {children}
       </body>
     </html>
